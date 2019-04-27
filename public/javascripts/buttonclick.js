1abb56871fd3cb6a62245e6d8c223acf9eea128e
Barkin Simsek <barkin.simsek@hotmail.com>
2019-04-27 13:40:37 +0400

    draft home page

---
+++ public/javascripts/buttonclick.js
@@ -0,0 +1,3 @@
+function buttonClick(buttonId) {
+  document.getElementById(buttonId).className = "button is-fullwidth is-success is-loading";
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
+  res.render('index', {page:'index', menuId:'index'});
+});
+
+router.get('/store', function(req, res, next) {
+  res.render('storeSubmit', {page:'store', menuId:'storeSubmit'});
 });
 
 module.exports = router;
